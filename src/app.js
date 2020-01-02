e88d585eaae84395159b5df84ba3f365a2c5350e
Sam Carlton <sam@sam.lc>
2020-01-02 14:49:59 -0600

    Resize book to match window

--- src/app.js
+++ src/app.js
@@ -4,106 +4,28 @@ import './turn'
  * Basic sample
 */
 
-export function addPage(page, book) {
 
-	var id, pages = book.turn('pages');
+const $flipbook = $('.flipbook')
 
-	// Create a new element for this page
-	var element = $('<div />', {});
+const calculateDimenstions = function () {
+	const basis = Number(getComputedStyle($flipbook[0]).getPropertyValue('--basis'))
 
-	// Add the page to the flipbook
-	if (book.turn('addPage', element, page)) {
-
-		// Add the initial HTML
-		// It will contain a loader indicator and a gradient
-		element.html('<div class="gradient"></div><div class="loader"></div>');
-
-		// Load the page
-		loadPage(page, element);
+	return {
+		width: 0.922 * basis * $(window).width(), 
+		height: 0.600 * basis * $(window).width()
 	}
-
 }
 
-export function loadPage(page, pageElement) {
+const { initialWidth, initialHeight } = calculateDimenstions()
 
-	// Create an image element
-
-	var img = $('<img />');
-
-	img.mousedown(function(e) {
-		e.preventDefault();
-	});
-
-	img.load(function() {
-		
-		// Set the size
-		$(this).css({width: '100%', height: '100%'});
-
-		// Add the image to the page after loaded
-
-		$(this).appendTo(pageElement);
-
-		// Remove the loader indicator
-		
-		pageElement.find('.loader').remove();
-	});
-
-	// Load the page
-
-	img.attr('src', 'pages/' +  page + '.jpg');
-
-}
-
-
-export function loadLargePage(page, pageElement) {
-	
-	var img = $('<img />');
-
-	img.load(function() {
-
-		var prevImg = pageElement.find('img');
-		$(this).css({width: '100%', height: '100%'});
-		$(this).appendTo(pageElement);
-		prevImg.remove();
-		
-	});
-
-	// Loadnew page
-	
-	img.attr('src', 'pages/' +  page + '-large.jpg');
-}
-
-
-export function loadSmallPage(page, pageElement) {
-	
-	var img = pageElement.find('img');
-
-	img.css({width: '100%', height: '100%'});
-
-	img.unbind('load');
-	// Loadnew page
-
-	img.attr('src', 'pages/' +  page + '.jpg');
-}
-
-
-
-// http://code.google.com/p/chromium/issues/detail?id=128488
-export function isChrome() {
-
-	return navigator.userAgent.indexOf('Chrome')!=-1;
-
-}
-
-
-$('.flipbook').turn({
+$flipbook.turn({
 	// Width
 
-	// width:922,
+	width: initialWidth,
 	
-	// // Height
+	// // // Height
 
-	// height:600,
+	height: initialHeight,
 
 	// Elevation
 
@@ -117,4 +39,12 @@ $('.flipbook').turn({
 
 	autoCenter: true
 
+})
+
+// Resize book when window resizes
+$( window ).resize(function() {
+	console.log('window resized')
+	const { width, height } = calculateDimenstions()
+	// $('.flipbook').turn("resize")
+	$flipbook.turn('size', width, height)
 })
